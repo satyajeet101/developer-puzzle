16e76b59d9ba1a3013010e996a648c65d6d6d98e
satyajeet singh <satyajeet.singh72@t-mobile.com>
2020-02-24 22:26:26 -0800

    Adding businness logic to refresh graph

--- apps/stocks/src/environments/environment.ts
+++ apps/stocks/src/environments/environment.ts
@@ -6,7 +6,7 @@ import { StocksAppConfig } from '@coding-challenge/stocks/data-access-app-config
 
 export const environment: StocksAppConfig = {
   production: false,
-  apiKey: '',
+  apiKey: 'Tpk_671a3876469841d589e9496fa11bd659',
   apiURL: 'https://sandbox.iexapis.com'
 };
 
